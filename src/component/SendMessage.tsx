2e15acd1511a3eae098e3278f662bd24690ad6e7
Anwesha <anweshadas909@gmail.com>
2024-09-23 07:21:34 +0530

    refac : issue fixed of movile view

--- src/component/SendMessage.tsx
+++ src/component/SendMessage.tsx
@@ -1,6 +1,7 @@
 import {FormEvent } from "react";
 import {IoSend} from "react-icons/io5";
 import {UseSendmessgae} from "../context/SendMessageContext.tsx";
+import "../Style/chatroom.css"
 
 export function SendMessage(){
     //const [value , setValue] = useState("")
@@ -21,11 +22,11 @@ export function SendMessage(){
     return(
         <>
             {chatRoom&&
-                <div className="bg-gray-500 rounded-l-lg w-full   px-10 py-5 shadow-sm flex " >
+                <div className="bg-gray-500 rounded-l-lg w-full messagebar  lg:px-10 md:px-5 py-5  shadow-sm flex " >
                     <form onSubmit={handleSubmit} className="container flex w-full">
                         <input value={value} onChange={e => setValue(e.target.value)} className="input  focus:outline-none w-full rounded-r-none"  type="text"/>
                         <button type="submit" className="bg-gray-200 w-auto rounded-r-lg text-black flex flex-row justify-center items-center">
-                            <IoSend className="mx-5 text-xl"/>
+                            <IoSend className="mx-5 text-xl md:text-2xl"/>
                         </button>
                     </form>
                 </div>
